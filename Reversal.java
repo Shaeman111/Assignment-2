6601b7758fa7830ba2e99e824739b4754cf1e66d
Shaden <shaden.pate.10@cnu.edu>
2015-10-19 17:32:56 -0400

    Signed-off-by: Shaden <shaden.pate.10@cnu.edu>

--- Reversal.java
+++ Reversal.java
@@ -1,9 +1,25 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-
+import java.util.Scanner;
 public class Reversal {
 
 	public static void reverseFile(File input, File output) throws FileNotFoundException {
-	
+		 try {
+
+		        Scanner scn = new Scanner(input);
+
+		        int lines = 0;
+		        while (scn.hasNextLine()) {
+		            scn.nextLine();
+		            lines++;
+		        }
+		        scn.close();
+		        scn = new Scanner(input);
+		        int lineAmount[] = new int[lines];
+		        //this is to replace (.size with .length in the future for loop)
+		 }
+		 catch (FileNotFoundException e) {
+		        e.printStackTrace();
+		    }
 }
 }
